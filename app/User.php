c8d7bb38792828b7bc49298b3ffebf5d8ac286e0
daisuke <dsaito85a@gmail.com>
2019-08-14 16:00:24 +0900

    UserにBillableを追加

--- app/User.php
+++ app/User.php
@@ -2,12 +2,15 @@
 
 namespace App;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
+
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use Billable;
 
     /**
      * The attributes that are mass assignable.
