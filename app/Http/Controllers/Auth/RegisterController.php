c52e55df33e42896838272e295d78af319f15e63
prostarting <kento-4.23.007@au.com>
2019-08-16 04:46:18 +0900

    ログイン後、登録後のリダイレクト先の修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
@@ -27,7 +28,17 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected function redirectTo() 
+    {
+        $user = Auth::user();
+
+        // roleによって、リダイレクト先を変える
+        if ($user->role === 1) {
+            return '/';
+        } else {
+            return '/credit';
+        }
+    }
 
     /**
      * Create a new controller instance.
