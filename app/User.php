d7347f64678982a05e9dd505e69753883c370a68
prostarting <kento-4.23.007@au.com>
2019-08-16 01:09:39 +0900

    管理画面におけるデータ表記の変更

--- app/User.php
+++ app/User.php
@@ -29,4 +29,28 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    // ステータスの表示を変更 (0 → 無効、1 → 有効)
+    public function getStatusTextAttribute()
+    {
+        switch($this->attributes['status']) {
+            case 1:
+                return '有効';
+            case 0:
+                return '無効';
+            default:
+                return '??';
+        }
+    }
+
+    // 日付の表示を変更 (2000-01-01 → 2000/01/01)
+    public function getCreatedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->attributes['created_at'])->format('Y/m/d H:i:s');
+    }
+    public function getUpdatedDueDateAttribute()
+    {
+        return Carbon::createFromFormat('Y-m-d H:i:s', $this->attributes['updated_at'])->format('Y/m/d H:i:s');
+    }
+    
 }
