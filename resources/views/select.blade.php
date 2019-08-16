fab192c16d635036ce38e0e0e3cece2c20992a6c
daisuke <dsaito85a@gmail.com>
2019-08-16 17:53:25 +0900

    不要ファイルの削除、cardをpanelにする(BootStrap4→3対応)

--- resources/views/select.blade.php
+++ resources/views/select.blade.php
@@ -4,40 +4,40 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-2">
-            <div class="card">
-                <div class="card-header">PLAN A</div>
-                <div class="card-body">
+            <div class="panel panel-info">
+                <div class="panel-heading">PLAN A</div>
+                <div class="panel-body">
                     <p>¥1,000</p>
                     <form action="/select" method="POST">
                         {{ csrf_field()}}
                         <input type="hidden" id="plan" name="plan" value="1000">
-                        <input type="submit" value="Get Start!" class="btn btn-outline-primary">
+                        <input type="submit" value="Get Start!" class="btn btn-primary">
                     </form>
                 </div>
             </div>
         </div>
         <div class="col-2">
-            <div class="card">
-                <div class="card-header">PLAN B</div>
-                <div class="card-body">
+            <div class="panel panel-info">
+                <div class="panel-heading">PLAN B</div>
+                <div class="panel-body">
                     <p>¥3,000</p>
                     <form action="/select" method="POST">
                         {{ csrf_field()}}
                         <input type="hidden" id="plan" name="plan" value="3000">
-                        <input type="submit" value="Get Start!" class="btn btn-outline-primary">
+                        <input type="submit" value="Get Start!" class="btn btn-primary">
                     </form>
                 </div>
             </div>
         </div>
         <div class="col-2">
-            <div class="card">
-                <div class="card-header">PLAN C</div>
-                <div class="card-body">
+            <div class="panel panel-info">
+                <div class="panel-heading">PLAN C</div>
+                <div class="panel-body">
                     <p>¥5,000</p>
                     <form action="/select" method="POST">
                         {{ csrf_field()}}
                         <input type="hidden" id="plan" name="plan" value="5000">
-                        <input type="submit" value="Get Start!" class="btn btn-outline-primary">
+                        <input type="submit" value="Get Start!" class="btn btn-primary">
                     </form>
 
                 </div>
