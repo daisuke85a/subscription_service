843243b0dd64fd89ce08730159122155a357334f
Danri <dkkabcd@gmail.com>
2019-08-16 15:06:57 +0900

    HomeController,web 修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -15,10 +15,9 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-
-          if ($_SERVER["REQUEST_URI"] !== '/') {
-              $this->middleware('auth');
-          }
+        if ($_SERVER["REQUEST_URI"] !== '/') {
+            $this->middleware('auth');
+        }
     }
 
     /**
@@ -26,46 +25,25 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
-    {
-        return view('home');
-    }
 
-    // TODO: HomeControllerを実装したら削除
-    /**
-     * Show the admin screen.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function show_admin_screen()
-    {
-        // ログイン中のユーザーを確認
-        $user = Auth::user();
-
-        // 管理者の場合のみ、管理画面に遷移できる
-        if ($user->role === 1) {
-            $users = User::all();
-            return view('admin')->with('users', $users);
-        } else {
-            return redirect('/');
-        }
-    }
-    
-    //TODO(だん)：ルートパースの判断
     public function all()
     {
         $user = Auth::user();
 
         if ($user) {
-            // 実際にカードを入力時にcard_brandカラムの値が入るので、
-            //card_brandカラムの値有無でカードを登録しているかどうかを判断
             $card = $user->card_brand;
 
-            if (!$card) {
-              return view('credit');
+            if ($user->role === 1) {
+                $users = User::all();
+
+                return view('admin')->with('users', $users);
             } else {
-              // 一般ユーザーの時nomalページがないので、homeにしました
-              return view('home');
+
+                if (!$card) {
+                    return view('credit');
+                } else {
+                    return view('normal');
+              }
             }
         } else {
             return view('select');
--- routes/web.php
+++ routes/web.php
@@ -1,68 +1,17 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-// Route::get('/', function () {
-//     // TODO: 本来は'/'にアクセスして、HomeController@indexにて表示画面を判定する
-//     // SubscriptionPlanの実装確認のため仮実装する
-//     return view('select');
-// });
-//TODO(だん)：ルートパースをコントローラーを通してから判断したいので書き換えました
 Route::get('/', 'HomeController@all');
 
-// TODO: 本来は'/'にアクセスして、HomeController@indexにて表示画面を判定する
-// SubscriptionPlanの実装確認のため仮実装する
-// Route::get('/select', function () {
-//     return view('select');
-// });
-
-// TODO: 本来は'/'にアクセスして、HomeController@indexにて表示画面を判定する
-// SubscriptionPlanの実装確認のため仮実装する
-// Route::get('/', function () {
-//     return view('select');
-// });
-
-// TODO: 本来は'/'にアクセスして、HomeController@indexにて表示画面を判定する
-// SubscriptionPlanの実装確認のため仮実装する
-Route::get('/normal', function () {
-    return view('normal');
-});
-
-
-// 管理者ユーザーがログインしたら、呼ばれる(管理画面を表示)
-Route::get('/admin', 'HomeController@show_admin_screen');
-
-use App\User;
-
-// TODO: 本来は'/'にアクセスして、HomeController@indexにて表示画面を判定する
-Route::get('/admin', function () {
-    $users = User::all();
-    return view('admin')->with('users', $users);
-});
-
-//サブスクリプションプランの選択がされたら呼ばれる
 Route::post('/select', 'SubscriptionController@select');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-// Stripeのクレジットカード情報の検証が完了したら呼ばれる
 Route::post('/stripe', 'SubscriptionController@create');
 
-// Stripeのサブスクプリションを退会する時に呼ばれる
 Route::post('/unsubscribe', 'SubscriptionController@unsubscribe');
 
-// クレジットカード情報の入力画面を表示する
 Route::get('/credit', function () {
     return view('credit');
 });
