42290fc6558dd6d850d21e470f08f0d237a25a85
daisuke <dsaito85a@gmail.com>
2019-08-16 17:40:14 +0900

    web.phpの不要コード削除、記載をわかりやすく修正

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,10 @@
 
 Route::get('/', 'HomeController@all');
 
-Route::post('/select', 'SubscriptionController@select');
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
+Route::post('/select', 'SubscriptionController@select');
 Route::post('/stripe', 'SubscriptionController@create');
-
 Route::post('/unsubscribe', 'SubscriptionController@unsubscribe');
 
 Route::get('/credit', function () {
