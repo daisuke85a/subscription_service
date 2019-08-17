49c077db7c1a057d1f286401b15b97d57f804160
Danri <dkkabcd@gmail.com>
2019-08-17 22:44:23 +0900

    cookieの位置を修正

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -12,47 +12,8 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-     class HomeController extends Controller
-     {
-         /**
-          * Create a new controller instance.
-          *
-          * @return void
-          */
-         public function __construct()
-         {
-             $url = Request::all();
-             var_dump($url);
-
-             if ($url !== '/') {
-                   $this->middleware('auth');
-               }
-         }
-
-         /**
-          * Show the application dashboard.
-          *
-          * @return \Illuminate\Http\Response
-          */
-
-         public functionTest all()
-         {
-             $user = Auth::user();
-
-             if ($user) {
-
-                 if ($user->role === 1) {
-                     $users = User::all();
-
-                     return view('admin')->with('users', $users);
-                 } else {
-                     return view('normal');
-                 }
-             } else {
-                 return view('select');
-             }
-         }
-
-     }
-
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
 }
